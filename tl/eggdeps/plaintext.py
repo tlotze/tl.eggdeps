934c4812a6fe61d89a9519e8cf98136f8cb187b9
Thomas Lotze <thomas@thomas-lotze.de>
2007-07-27 16:15:22 +0200

    cleaned up plain text printing, added marker for dead ends

--- tl/eggdeps/plaintext.py
+++ tl/eggdeps/plaintext.py
@@ -2,18 +2,17 @@
 # See also LICENSE.txt
 
 
-def print_subgraph(root, shortest, has_tree, printed, nodes,
+def print_subgraph(root, shortest, has_tree, printed, nodes, is_dead_end,
                    link_type=None, depth=0):
     print_tree = depth == shortest[root] and root not in printed
-    if print_tree or root not in has_tree:
-        ellipsis = ""
-    else:
-        ellipsis = " ..."
+    line = depth*"    " + root
     if link_type:
-        link_prefix = " (%s)" % link_type
-    else:
-        link_prefix = ""
-    print depth*"    " + root + link_prefix + ellipsis
+        line += " (%s)" % link_type
+    if not print_tree and root in has_tree:
+        line += " ..."
+    if is_dead_end(root):
+        line += " *"
+    print line
 
     if not print_tree:
         return
@@ -23,7 +22,7 @@ def print_subgraph(root, shortest, has_tree, printed, nodes,
     for link_type, targets in sorted(links.iteritems()):
         for target in sorted(targets):
             print_subgraph(target, shortest, has_tree, printed, nodes,
-                           link_type, depth + 1)
+                           is_dead_end, link_type, depth + 1)
 
 
 def prepare(root, nodes, shortest, has_tree, depth=0):
@@ -47,4 +46,4 @@ def print_graph(graph, is_dead_end):
     printed = set()
 
     for root in roots:
-        print_subgraph(root, shortest, has_tree, printed, nodes)
+        print_subgraph(root, shortest, has_tree, printed, nodes, is_dead_end)
