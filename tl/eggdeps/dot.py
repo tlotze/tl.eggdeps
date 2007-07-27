5ceb9bdd10697be1de4f4c016b881c02d09364ef
Thomas Lotze <thomas@thomas-lotze.de>
2007-07-27 14:29:56 +0200

    actually output a dot file

--- tl/eggdeps/dot.py
+++ tl/eggdeps/dot.py
@@ -2,5 +2,33 @@
 # See also LICENSE.txt
 
 
-def print_dot(graph):
+def _print_dot(graph):
     roots, nodes = graph
+
+    print "digraph {"
+
+    for name, links in nodes.iteritems():
+        node_options = dict(label=name)
+        if name in roots:
+            node_options["style"] = "filled"
+        print name.replace(".", "_") + format_options(node_options)
+
+        for link_type, targets in links.iteritems():
+            edge_options = {}
+            if link_type is 'extra':
+                edge_options["color"] = "lightgrey"
+
+            for target in targets:
+                print "%s -> %s%s" % (name.replace(".", "_"),
+                                      target.replace(".", "_"),
+                                      format_options(edge_options))
+
+    print "}"
+
+
+def format_options(options):
+    if not options:
+        return ""
+
+    return " [%s]" % ", ".join('%s="%s"' % item
+                               for item in options.iteritems())
