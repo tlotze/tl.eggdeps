527caf4c9f58a4fccad4388b1a1c4e3ac59f188b
Thomas Lotze <thomas@thomas-lotze.de>
2009-07-12 13:40:40 +0200

    simplified test setup by setting default options on the Options class itself

--- tl/eggdeps/tests.py
+++ tl/eggdeps/tests.py
@@ -45,6 +45,12 @@ def sort_specs(specs):
 
 class Options(dict):
 
+    version_numbers = False
+    once = False
+    terse = False
+    cluster = False
+    version_specs = False
+
     def __init__(self, *args, **kwargs):
         self.__dict__ = self
         super(Options, self).__init__(*args, **kwargs)
